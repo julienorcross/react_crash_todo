77ce7a0b6c141914f878af11cbc64e3e3ca74db9
Julie Norcross <julesnorc@gmail.com>
2019-06-09 11:36:34 -0400

    Updates

---
+++ precache-manifest.4917ef16666c89bf37ca7b9ae0fbd5d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df7c5ec1dd2e31b111975b40a538103",
+    "url": "/react_crash_todo/index.html"
+  },
+  {
+    "revision": "64a309efee0d069e77e6",
+    "url": "/react_crash_todo/static/css/main.76e4443e.chunk.css"
+  },
+  {
+    "revision": "000fff294fb0e3d88c6f",
+    "url": "/react_crash_todo/static/js/2.e02322d4.chunk.js"
+  },
+  {
+    "revision": "64a309efee0d069e77e6",
+    "url": "/react_crash_todo/static/js/main.ed30e616.chunk.js"
+  },
+  {
+    "revision": "1cc9b059203060ac9057",
+    "url": "/react_crash_todo/static/js/runtime~main.045ec42f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-crash-todo/precache-manifest.8e5d81ec0496b42284343749ea1901e3.js"
+  "/react_crash_todo/precache-manifest.4917ef16666c89bf37ca7b9ae0fbd5d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-crash-todo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_crash_todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
